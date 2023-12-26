7044418d1b4039027e775c6768cb2d7f22094a66
Suryaraj Timsina <devopsuryaraj@gmail.com>
2023-12-26 01:36:03 +0000

    adding chagnes to featureb branch

--- myfirstrepo.sh
+++ myfirstrepo.sh
@@ -3,5 +3,10 @@
 echo "We are starting our new git project"
 
 echo "This is a newly added line"
-
+df -h
 echo "Good morning, DevOps!"
+
+if [ -f /var/run/apache2/apache.pid ]; then
+echo "Apache servie is running"
+
+fi
